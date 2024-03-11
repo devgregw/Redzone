516699306c80c69aa4f0b33b4b71260326fe907b
Greg Whatley <me@gregwhatley.dev>
2024-03-11 13:01:42 -0500

    Add SSL config

--- SPCServer/Sources/SPCServer/configure.swift
+++ SPCServer/Sources/SPCServer/configure.swift
@@ -1,9 +1,20 @@
+import NIOSSL
 import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
     app.http.server.configuration.address = .hostname(nil, port: 8081)
     app.http.server.configuration.responseCompression = .enabled
+    
+    do {
+        app.http.server.configuration.tlsConfiguration = .makeServerConfiguration(
+            certificateChain: try NIOSSLCertificate.fromPEMFile("/cert/chain.pem").map { .certificate($0) },
+            privateKey: .file("/cert/privkey.pem")
+        )
+        app.logger.info("SSL configuration successful")
+    } catch {
+        app.logger.error("SSL configuration failed: \(error.localizedDescription)")
+    }
     
     // register routes
     try routes(app)
