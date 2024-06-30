2626ade2cedbdc68dec52f60cb29aa2323aeba64
Greg Whatley <me@gregwhatley.dev>
2024-06-30 10:46:13 -0500

    Move CLLocationCoordinate2D extension

--- SPCMap/Shared/Settings.swift
+++ SPCMap/Shared/Settings.swift
@@ -15,27 +15,6 @@ class Settings {
     static var lastKnownLocation: CLLocationCoordinate2D?
 }
 
-extension CLLocationCoordinate2D: Codable {
-    enum CodingKeys: String, CodingKey {
-        case latitude
-        case longitude
-    }
-    
-    public func encode(to encoder: any Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(latitude, forKey: .latitude)
-        try container.encode(longitude, forKey: .longitude)
-    }
-    
-    public init(from decoder: any Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.init(
-            latitude: try container.decode(CLLocationDegrees.self, forKey: .latitude),
-            longitude: try container.decode(CLLocationDegrees.self, forKey: .longitude)
-        )
-    }
-}
-
 enum SettingsKey: String {
     case lastKnownLocation
 }
