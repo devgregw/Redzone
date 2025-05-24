1c1517cdf80027afc3be51d1cfd27d2ef706d3b7
Greg Whatley <me@gregwhatley.dev>
2025-05-23 22:39:18 -0500

    Schedule function (#8)
    * Clean up workflows
    
    * Schedule function

--- redzone-web/functions/src/index.ts
+++ redzone-web/functions/src/index.ts
@@ -1,5 +1,4 @@
-// import { onSchedule } from "firebase-functions/v2/scheduler";
-import { onRequest } from "firebase-functions/v2/https";
+import { onSchedule } from "firebase-functions/v2/scheduler";
 import * as logger from "firebase-functions/logger";
 import { getDatabase } from "firebase-admin/database";
 import { initializeApp } from "firebase-admin/app";
@@ -7,24 +6,25 @@ import { initializeApp } from "firebase-admin/app";
 initializeApp()
 
 // exports.cleanup = onSchedule('every day 00:00', async () => {
-export const cleanup = onRequest(async (_, res) => {
-    logger.debug('Running database cleanup')
+export const cleanup = onSchedule('every day 00:00', async () => {
+    const now = new Date()
+    logger.debug(`Running database cleanup at ${now.toLocaleString()}`)
     const db = getDatabase()
     const manifestRef = db.ref('manifest')
     const manifest: Record<string, number> = (await manifestRef.get()).val()
     logger.info('Manifest', manifest)
     const keysToDelete: string[] = []
 
-    const now = Date.now()
+    
     for (const key in manifest) {
         const timestamp = new Date(manifest[key]).getTime()
-        if (Math.abs(now - timestamp) >= 86_400_000) {
+        if (Math.abs(now.getTime() - timestamp) >= 86_400_000) {
             delete manifest[key]
             keysToDelete.push(key.replaceAll('__', '/'))
         }
     }
 
-    logger.info('Deleting stale entries', keysToDelete)
+    logger.info(`Deleting ${keysToDelete.length} stale entries`, keysToDelete)
 
     try {
         await Promise.all(keysToDelete.map(k => db.ref(k).remove()))
@@ -38,9 +38,5 @@ export const cleanup = onRequest(async (_, res) => {
         logger.error('Failed to update the manifest', error)
     }
 
-    logger.debug('Done')
-    res.status(200).send({
-        manifest,
-        keysToDelete
-    })
+    logger.debug(`Completed at ${new Date().toLocaleString()}`)
 })
