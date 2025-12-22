9779a74c2bec7c02ad75689f4f14a860be96483a
Greg Whatley <me@gregwhatley.dev>
2025-12-21 23:17:58 -0600

    update scripts

--- Redzone-iOS/BuildSupport/periphery.sh
+++ Redzone-iOS/BuildSupport/periphery.sh
@@ -2,4 +2,10 @@
 
 set -euo pipefail
 
-periphery scan --skip-build --relative-results --format github-actions --index-store-path "$RUNNER_TEMP/DerivedData" # Periphery will read the config YAML file and merge it with these CLI args
+# Periphery will read the config YAML file and merge it with these CLI args
+periphery \
+    scan \
+    --skip-build \
+    --relative-results \
+    --format github-actions \
+    --index-store-path "$RUNNER_TEMP/DerivedData"
--- Redzone-iOS/BuildSupport/resolve-packages.sh
+++ Redzone-iOS/BuildSupport/resolve-packages.sh
@@ -2,4 +2,7 @@
 
 set -euo pipefail
 
-xcodebuild -resolvePackageDependencies -disableAutomaticPackageResolution -clonedSourcePackagesDirPath "$RUNNER_TEMP/clonedSourcePackages" 2>&1 | xcbeautify --renderer github-actions
+xcodebuild \
+    -resolvePackageDependencies \
+    -disableAutomaticPackageResolution \
+    -clonedSourcePackagesDirPath "$RUNNER_TEMP/clonedSourcePackages" 2>&1 | xcbeautify --renderer github-actions
