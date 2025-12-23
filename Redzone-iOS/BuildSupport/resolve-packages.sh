5854b2e942852f3dc074976ded011e0388884133
Greg Whatley <me@gregwhatley.dev>
2025-12-22 18:18:00 -0600

    refactor actions

--- Redzone-iOS/BuildSupport/resolve-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-set -euo pipefail
-
-xcodebuild \
-    -resolvePackageDependencies \
-    -disableAutomaticPackageResolution \
-    -clonedSourcePackagesDirPath "$RUNNER_TEMP/clonedSourcePackages" 2>&1 | xcbeautify --renderer github-actions
