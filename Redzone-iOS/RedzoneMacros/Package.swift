5eb395d2c839febd19f0f1e656fec675acfa5ea9
Greg Whatley <me@gregwhatley.dev>
2026-01-17 21:36:33 -0600

    Use SwiftLint plugin instead of script (#56)
    * Replace SwiftLint build script with plugin
    
    * Update actions
    
    * verbose
    
    * Xcode project recommendations
    
    * Revert "verbose"
    
    This reverts commit 96834965394e1a719d62a27d899a5457a60a0d10.
    
    * Add test plans, disable UI tests
    
    * Add watchOS build/test step
    
    * Add auto device check
    
    * Update Redzone.base.xcconfig

--- Redzone-iOS/RedzoneMacros/Package.swift
+++ Redzone-iOS/RedzoneMacros/Package.swift
@@ -6,7 +6,7 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "RedzoneMacros",
-    platforms: [.macOS(.v11), .iOS(.v14), .watchOS(.v7)],
+    platforms: [.macOS(.v12), .iOS(.v14), .watchOS(.v7)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,7 +15,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0-latest"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", .upToNextMajor(from: "602.0.0")),
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins.git", .upToNextMinor(from: "0.63.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -26,10 +27,19 @@ let package = Package(
             dependencies: [
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ],
+            plugins: [
+                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
             ]
         ),
 
         // Library that exposes a macro as part of its API, which is used in client programs.
-        .target(name: "RedzoneMacros", dependencies: ["RedzoneMacrosInternal"])
+        .target(
+            name: "RedzoneMacros",
+            dependencies: ["RedzoneMacrosInternal"],
+            plugins: [
+                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
+            ]
+        )
     ]
 )
