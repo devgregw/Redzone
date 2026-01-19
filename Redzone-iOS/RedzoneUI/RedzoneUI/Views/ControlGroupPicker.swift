9d36b3d3b1c6b6c21cec5cb6d20b604b6906add5
Greg Whatley <me@gregwhatley.dev>
2026-01-19 13:14:52 -0600

    Update ControlGroupPicker.swift

--- Redzone-iOS/RedzoneUI/RedzoneUI/Views/ControlGroupPicker.swift
+++ Redzone-iOS/RedzoneUI/RedzoneUI/Views/ControlGroupPicker.swift
@@ -9,22 +9,18 @@ import SwiftUI
 
 @available(watchOS, unavailable)
 public struct ControlGroupPicker<Value: Hashable & Sendable, Content: View>: View {
-    let title: String
+    let titleKey: LocalizedStringResource
     @Binding var selection: Value
     let content: Content
 
     public init(_ titleKey: LocalizedStringResource, selection: Binding<Value>, @ViewBuilder content: () -> Content) {
-        self.init(.init(localized: titleKey), selection: selection, content: content)
-    }
-
-    public init(_ title: String, selection: Binding<Value>, @ViewBuilder content: () -> Content) {
-        self.title = title
+        self.titleKey = titleKey
         self._selection = selection
         self.content = content()
     }
 
     public var body: some View {
-        ControlGroup(title) {
+        ControlGroup(titleKey) {
             ForEach(subviews: content) { subview in
                 if let tag = subview.containerValues.tag(for: Value.self) {
                     Toggle(isOn: $selection.mapEquals(tag)) {
