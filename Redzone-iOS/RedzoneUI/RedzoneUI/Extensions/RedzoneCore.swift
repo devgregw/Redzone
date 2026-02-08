244902c24688c275638c9bc02c9c4be142aa619b
Greg Whatley <me@gregwhatley.dev>
2026-02-07 21:32:18 -0600

    Add fire wx definitions

--- Redzone-iOS/RedzoneUI/RedzoneUI/Extensions/RedzoneCore.swift
+++ Redzone-iOS/RedzoneUI/RedzoneUI/Extensions/RedzoneCore.swift
@@ -12,6 +12,7 @@ extension OutlookType: @retroactive CustomStringConvertible, @retroactive Custom
     public var localizedStringResource: LocalizedStringResource {
         switch self {
         case let .convective(value): value.localizedStringResource
+        case let .fire(value): value.localizedStringResource
         @unknown default: LocalizedStringResource(stringLiteral: String(describing: self))
         }
     }
@@ -23,6 +24,16 @@ extension Convective: @retroactive CustomStringConvertible, @retroactive CustomL
         case let .day1(classification), let .day2(classification): .convectiveDescription(label: classification.description, day: day)
         case .day3(probabilistic: false): .convectiveDescription(label: Classification.categorical.description, day: day)
         case .day3(probabilistic: true), .day4, .day5, .day6, .day7, .day8: .convectiveDescription(label: String(localized: .probabilistic), day: day)
+        @unknown default: LocalizedStringResource(stringLiteral: String(describing: self))
+        }
+    }
+}
+
+extension Fire: @retroactive CustomStringConvertible, @retroactive CustomLocalizedStringResourceConvertible {
+    public var localizedStringResource: LocalizedStringResource {
+        switch self {
+        case .day1: .convectiveDescription(label: "Fire", day: 1)
+        case .day2: .convectiveDescription(label: "Fire", day: 2)
         @unknown default: LocalizedStringResource(stringLiteral: String(describing: self))
         }
     }
